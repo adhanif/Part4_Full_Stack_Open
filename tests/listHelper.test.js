7b14e0fa5a26733bc0a4726652980f2c1e3bea00
ad.hanif <ad.hanif@outlook.com>
2023-09-03 13:24:46 +0200

    4.5*: helper functions and unit tests, step3completed

--- tests/listHelper.test.js
+++ tests/listHelper.test.js
@@ -17,6 +17,24 @@ describe("total likes", () => {
       likes: 7,
       __v: 0,
     },
+  ];
+
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    expect(result).toBe(7);
+  });
+});
+
+describe("most favourite likes", () => {
+  const listWithOneBlog1 = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0,
+    },
     {
       _id: "5a422aa71b54a676234d17f8",
       title: "Go To Statement Considered Harmful",
@@ -59,8 +77,12 @@ describe("total likes", () => {
     },
   ];
 
-  test("when list has only one blog, equals the likes of that", () => {
-    const result = listHelper.totalLikes(listWithOneBlog);
-    expect(result).toBe(36);
+  test("which blog has the most likes", () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog1);
+    expect(result).toEqual({
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      likes: 12,
+    });
   });
 });
