ff9a3c4c5293077ea9539b5b5104066121fa15eb
ad.hanif <ad.hanif@outlook.com>
2023-09-05 17:59:02 +0200

    Blog list tests, step2 completed

--- models/blogsSchema.js
+++ models/blogsSchema.js
@@ -7,6 +7,14 @@ const blogsSchema = new mongoose.Schema({
   likes: Number,
 });
 
+blogsSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
 const Blog = mongoose.model("Blog", blogsSchema);
 
 module.exports = Blog;
